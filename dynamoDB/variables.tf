f7a352cf692afdef5dfaf28dcfd32fec3de7f001
Abderrahmen ISSA <abderrahmenissa@macbook-pro-de-abderrahmen.home>
2024-05-27 17:28:47 +0200

    Test DynamoDB module

--- dynamoDB/variables.tf
+++ dynamoDB/variables.tf
@@ -3,6 +3,6 @@ variable "DB_TABLE_NAME" {}
 variable "APP_NAME" {}
 variable "AWS_REGION" {}
 variable "AUTOSCALE_MIN_READ_CAPACITY" {}
-variable "AUTOSCALE_MAX_WRITE_CAPACITY" {}
+variable "AUTOSCALE_MAX_READ_CAPACITY" {}
 variable "AUTOSCALE_MIN_WRITE_CAPACITY" {}
 variable "AUTOSCALE_MAX_WRITE_CAPACITY" {}
