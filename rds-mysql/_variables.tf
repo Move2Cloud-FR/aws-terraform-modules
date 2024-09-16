63549815e8262a696c88a195ab8521ffbfa2036b
Abderrahmen ISSA <contact@move2cloud.fr>
2024-09-16 21:35:48 +0200

    Add some output

--- rds-mysql/_variables.tf
+++ rds-mysql/_variables.tf
@@ -8,7 +8,6 @@ variable "ENV_PREFIX" {}
 
 ########################### RDS Config ###############################
 variable "DATABASE_USER" {}
-variable "DATABASE_PASSWORD" {}
 variable "DATABASE_NAME" {}
 variable "DATABASE_INSTANCE_CLASS" {}
 variable "AUTHORISED_CIDR_BLOCKS" {}
