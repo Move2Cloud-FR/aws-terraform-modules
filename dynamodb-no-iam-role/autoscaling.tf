62ddb34f62704a21b47ec7a83e1c16a8964b6517
Abderrahmen ISSA <40058504+AbderrahmenISSA@users.noreply.github.com>
2024-10-24 16:52:15 +0200

    Update autoscaling.tf

--- dynamodb-no-iam-role/autoscaling.tf
+++ dynamodb-no-iam-role/autoscaling.tf
@@ -63,7 +63,7 @@ resource "aws_appautoscaling_target" "read_target" {
   resource_id        = "table/${aws_dynamodb_table.dbtable.name}"
   scalable_dimension = "dynamodb:table:ReadCapacityUnits"
   service_namespace  = "dynamodb"
-  role_arn           = var.IAM_ROLE_ARN
+  role_arn           = aws_iam_role.auto_scale_role.arn
 }
 
 resource "aws_appautoscaling_policy" "read_policy" {
@@ -88,7 +88,7 @@ resource "aws_appautoscaling_target" "write_target" {
   resource_id        = "table/${aws_dynamodb_table.dbtable.name}"
   scalable_dimension = "dynamodb:table:WriteCapacityUnits"
   service_namespace  = "dynamodb"
-  role_arn           = var.IAM_ROLE_ARN
+  role_arn           = aws_iam_role.auto_scale_role.arn
 }
 
 resource "aws_appautoscaling_policy" "write_policy" {
