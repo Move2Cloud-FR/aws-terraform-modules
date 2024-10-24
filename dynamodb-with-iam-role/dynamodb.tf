3d9d595718996578f1af6936260da5701896df19
Abderrahmen ISSA <contact@move2cloud.fr>
2024-10-24 16:54:01 +0200

    Use IAM role predifined

--- dynamodb-with-iam-role/dynamodb.tf
+++ dynamodb-with-iam-role/dynamodb.tf
@@ -32,3 +32,55 @@ resource "aws_dynamodb_table" "dbtable" {
     ]
   }
 }
+
+// add autoscaling
+resource "aws_appautoscaling_target" "read_target" {
+  max_capacity       = var.AUTOSCALE_MAX_READ_CAPACITY
+  min_capacity       = var.AUTOSCALE_MIN_READ_CAPACITY
+  resource_id        = "table/${aws_dynamodb_table.dbtable.name}"
+  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+  service_namespace  = "dynamodb"
+  role_arn           = var.IAM_ROLE_ARN
+}
+
+resource "aws_appautoscaling_policy" "read_policy" {
+  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.read_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.read_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.read_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.read_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+
+    target_value = var.AUTOSCALE_READ_TARGET
+  }
+}
+
+resource "aws_appautoscaling_target" "write_target" {
+  max_capacity       = var.AUTOSCALE_MAX_WRITE_CAPACITY
+  min_capacity       = var.AUTOSCALE_MIN_WRITE_CAPACITY
+  resource_id        = "table/${aws_dynamodb_table.dbtable.name}"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+  role_arn           = var.IAM_ROLE_ARN
+}
+
+resource "aws_appautoscaling_policy" "write_policy" {
+  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.write_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.write_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.write_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.write_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+
+    target_value = var.AUTOSCALE_WRITE_TARGET
+  }
+}
+
