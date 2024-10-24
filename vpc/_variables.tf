7f0da2350332ceeda6c805be37a830ce69c1365a
Abderrahmen ISSA <40058504+AbderrahmenISSA@users.noreply.github.com>
2024-10-24 15:21:46 +0200

    Vpc split (#4)
    * Split vpc into 3 subnets
    
    * Split vpc into 3 subnets
    
    ---------
    
    Co-authored-by: Abderrahmen ISSA <contact@move2cloud.fr>

--- vpc/_variables.tf
+++ vpc/_variables.tf
@@ -2,8 +2,8 @@
 variable "AWS_REGION" {}
 variable "VPC_CIDR" {}
 variable "PUBLIC_SUBNET_CIDRS" {}
-variable "PRIVATE_APP_SUBNET_CIDRS" {}
-variable "PRIVATE_DB_SUBNET_CIDRS" {}
+variable "PRIVATE_BACKEND_NAT_GAT_SUBNET_CIDRS" {}
+variable "PRIVATE_BACKEND_SUBNET_CIDRS" {}
 variable "SUBNET_AZS" {}
 
 ############################### Project name ##################################
