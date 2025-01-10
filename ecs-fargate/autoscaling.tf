d3d0e890169d6e601321be2260f03e9a89f751d2
Mariem-SSI <msoussi@move2cloud.fr>
2025-01-10 16:25:36 +0100

    add scheduled AS

--- ecs-fargate/autoscaling.tf
+++ ecs-fargate/autoscaling.tf
@@ -2,11 +2,51 @@ resource "aws_appautoscaling_target" "TARGET" {
   service_namespace  = "ecs"
   resource_id        = "service/${var.ECS_CLUSTER}/${aws_ecs_service.ECS_SERVICE.name}"
   scalable_dimension = "ecs:service:DesiredCount"
-  min_capacity       = 1
+  min_capacity       = 1  #est c q j peux mettre 0 ici ? le nbre d tasks commence avec la desired capacity puis s'adapte
   max_capacity       = 3
 }
 
-# Automatically scale capacity up by one
+###########################################################################################
+
+resource "aws_appautoscaling_scheduled_action" "morning_scale_out" {
+  
+  count = var.ENV_PREFIX == "dev" ? 1 : 0 #A vérifier si l'environnemnt et dans env_prefix
+  
+  name               = "morning_scale_out"
+  service_namespace  = "ecs"
+  resource_id        = "service/${var.ECS_CLUSTER}/${aws_ecs_service.ECS_SERVICE.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  schedule           = "cron(0 9 ? * MON-FRI *)"
+  timezone           = "Europe/London"
+
+  scalable_target_action {
+    min_capacity = 1  #var.scheduled_max_capacity 
+    max_capacity = 1  #var.scheduled_max_capacity # 1
+  }
+}
+
+resource "aws_appautoscaling_scheduled_action" "evening_scale_in" {
+
+  count = var.ENV_PREFIX == "dev" ? 1 : 0
+
+  name               = "evening_scale_in"
+  service_namespace  = "ecs"
+  resource_id        = "service/${var.ECS_CLUSTER}/${aws_ecs_service.ECS_SERVICE.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  schedule           = "cron(0 19 ? * MON-FRI *)"
+  timezone           = "Europe/London"
+
+  scalable_target_action {
+    min_capacity = 0  #var.scheduled_min_capacity 
+    max_capacity = 0  #var.scheduled_min_capacity 
+  }
+
+  depends_on = [aws_appautoscaling_scheduled_action.morning_scale_out]
+}
+
+###########################################################################################
+
+# Automatically scale capacity up - tiggered by CW alarm
 resource "aws_appautoscaling_policy" "UP" {
   name               = "cb_scale_up"
   service_namespace  = "ecs"
@@ -27,7 +67,7 @@ resource "aws_appautoscaling_policy" "UP" {
   depends_on = [aws_appautoscaling_target.TARGET]
 }
 
-# Automatically scale capacity down by one
+# Automatically scale capacity down by one - tiggered by CW alarm
 resource "aws_appautoscaling_policy" "DOWN" {
   name               = "cb_scale_down"
   service_namespace  = "ecs"
