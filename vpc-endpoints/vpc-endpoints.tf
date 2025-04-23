c7eb4b3fc0f46d817d40f9ea7998a7bfc6c7a9d9
Mariem-SSI <msoussi@move2cloud.fr>
2025-04-23 12:37:48 +0200

    update vpc-endpoint.tf

--- vpc-endpoints/vpc-endpoints.tf
+++ vpc-endpoints/vpc-endpoints.tf
@@ -1,12 +1,11 @@
-resource "aws_vpc_endpoint" "lambda" {
-  vpc_id       = var.VPC_ID
-  service_name = "com.amazonaws.${var.AWS_REGION}.lambda"
-  vpc_endpoint_type = "Interface"
-  subnet_ids   = var.PRIVATE_SUBNETS_IDS
-  security_group_ids = [aws_security_group.vpc_endpoint_sg.id]
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id           = var.VPC_ID
+  service_name     = "com.amazonaws.${var.AWS_REGION}.dynamodb"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids  = var.ROUTE_TABLE_IDS
 
   tags = {
-    Name        = "${var.APP_NAME}-${var.ENV_PREFIX}-lambda-endpoint"
+    Name        = "${var.APP_NAME}-${var.ENV_PREFIX}-dynamodb-endpoint"
     Environment = var.ENV_PREFIX
   }
 }
@@ -16,6 +15,7 @@ resource "aws_vpc_endpoint" "ecr_api" {
   service_name = "com.amazonaws.${var.AWS_REGION}.ecr.api"
   vpc_endpoint_type = "Interface"
   subnet_ids   = var.PRIVATE_SUBNETS_IDS
+  private_dns_enabled = true
   security_group_ids = [aws_security_group.vpc_endpoint_sg.id]
 
   tags = {
@@ -29,6 +29,7 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
   service_name = "com.amazonaws.${var.AWS_REGION}.ecr.dkr"
   vpc_endpoint_type = "Interface"
   subnet_ids   = var.PRIVATE_SUBNETS_IDS
+  private_dns_enabled = true
   security_group_ids = [aws_security_group.vpc_endpoint_sg.id]
 
   tags = {
@@ -68,17 +69,10 @@ resource "aws_security_group" "vpc_endpoint_sg" {
   vpc_id      = var.VPC_ID
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Ou référencez le SG des tâches ECS
   }
 
   tags = {
