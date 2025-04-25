67d9ff4502481de2e96fe6b94f8bb8b02f19350d
Mariem-SSI <msoussi@move2cloud.fr>
2025-04-25 16:18:03 +0200

    update variables format in vpc-endpoints folder

--- vpc-endpoints/vpc-endpoints.tf
+++ vpc-endpoints/vpc-endpoints.tf
@@ -2,7 +2,7 @@ resource "aws_vpc_endpoint" "dynamodb" {
   vpc_id           = var.VPC_ID
   service_name     = "com.amazonaws.${var.AWS_REGION}.dynamodb"
   vpc_endpoint_type = "Gateway"
-  route_table_ids  = var.ROUTE_TABLE_IDS
+  route_table_ids  = local.route_table_ids
 
   tags = {
     Name        = "${var.APP_NAME}-${var.ENV_PREFIX}-dynamodb-endpoint"
@@ -14,7 +14,7 @@ resource "aws_vpc_endpoint" "ecr_api" {
   vpc_id       = var.VPC_ID
   service_name = "com.amazonaws.${var.AWS_REGION}.ecr.api"
   vpc_endpoint_type = "Interface"
-  subnet_ids   = var.PRIVATE_SUBNETS_IDS
+  subnet_ids   = local.private_subnets
   private_dns_enabled = true
   security_group_ids = [aws_security_group.vpc_endpoint_sg.id]
 
@@ -28,7 +28,7 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
   vpc_id       = var.VPC_ID
   service_name = "com.amazonaws.${var.AWS_REGION}.ecr.dkr"
   vpc_endpoint_type = "Interface"
-  subnet_ids   = var.PRIVATE_SUBNETS_IDS
+  subnet_ids   = local.private_subnets
   private_dns_enabled = true
   security_group_ids = [aws_security_group.vpc_endpoint_sg.id]
 
@@ -43,7 +43,7 @@ resource "aws_vpc_endpoint" "cloudwatch" {
   service_name = "com.amazonaws.${var.AWS_REGION}.logs"
   vpc_endpoint_type = "Interface"
   private_dns_enabled = true
-  subnet_ids   = var.PRIVATE_SUBNETS_IDS
+  subnet_ids   = local.private_subnets
   security_group_ids = [aws_security_group.vpc_endpoint_sg.id]
 
   tags = {
@@ -56,7 +56,7 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id       = var.VPC_ID
   service_name = "com.amazonaws.${var.AWS_REGION}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids = var.ROUTE_TABLE_IDS
+  route_table_ids = local.route_table_ids
 
   tags = {
     Name        = "${var.APP_NAME}-${var.ENV_PREFIX}-s3-endpoint"
@@ -80,4 +80,10 @@ resource "aws_security_group" "vpc_endpoint_sg" {
     Name        = "${var.APP_NAME}-${var.ENV_PREFIX}-vpc-endpoint-sg"
     Environment = var.ENV_PREFIX
   }
+}
+
+# Variables Format from string to List of strings
+locals {
+  private_subnets = split(", ", var.PRIVATE_SUBNETS_IDS)
+  route_table_ids = split(", ", var.ROUTE_TABLE_IDS)
 }
